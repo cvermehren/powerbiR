1f3e5b34f2d092d86deed01fc4f01f6ddeebb662
cvermehren <cv@cantab.net>
2021-12-27 11:59:07 +0000

    Add more tests

--- tests/testthat/test-pbi_schema_types_infer.R
+++ tests/testthat/test-pbi_schema_types_infer.R
@@ -1,3 +1,27 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Logical vector is infered as 'Boolean'", {
+  expect_equal(pbi_schema_types_infer(c(TRUE, FALSE)), "Boolean")
+})
+
+test_that("Integer vector is infered as 'Int64'", {
+  expect_equal(pbi_schema_types_infer(as.integer(c(1, 2))), "Int64")
+})
+
+test_that("Double vector is infered as 'Double'", {
+  expect_equal(pbi_schema_types_infer( as.double(c(1, 2))), "Double")
+})
+
+test_that("Date vector is infered as 'Date'", {
+  expect_equal(pbi_schema_types_infer( as.Date(c("2021-01-01", "2021-01-02"))), "DateTime")
+})
+
+test_that("Date vector is infered as 'Date'", {
+  expect_equal(pbi_schema_types_infer( Sys.Date()), "DateTime")
+})
+
+test_that("Time vector is infered as 'Date'", {
+  expect_equal(pbi_schema_types_infer( Sys.time()), "DateTime")
+})
+
+test_that("Factor vector is infered as 'String'", {
+  expect_equal(pbi_schema_types_infer( factor(c("Man", "Woman"))), "String")
 })
