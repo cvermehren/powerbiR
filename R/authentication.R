54c4d91d87e7378d4eb4d34954bfbcbe6ebaa80a
cvermehren <cv@cantab.net>
2021-12-31 07:33:39 +0000

    update auth documentation

--- R/authentication.R
+++ R/authentication.R
@@ -2,27 +2,49 @@
 
 #' Authenticate your Power BI API session
 #'
-#' Supply your application name, client ID and client secret. You will need to
-#' register an application here:
-#' https://powerbi.microsoft.com/en-us/documentation/powerbi-developer-register-a-web-app/
+#' Run this function at start to obtain an authentication token. The
+#' token will be saved to your session and refreshed automatically upon
+#' expiration.
 #'
 #' @param tenant Your Microsoft tenant ID
 #' @param app Your Microsoft App ID
 #' @param password Your Microsoft App password
 #'
-#' @return An active token
+#' @details A wrapper for AzureAuth::get_azure_token(). Currently, only
+#' non-interactive authentication is supported. You need to register an Azure
+#' Active Directory service-principal application and obtain tenant ID, app ID
+#' and app password (client secret).
+#'
+#' Please notice that for reasons of CRAN policy, the first time AzureAuth is
+#' loaded, it will prompt you for permission to create a user-specific directory
+#' in order to cache the token. The prompt only appears in an interactive
+#' session, not in a batch script.
+#'
+#' @return Invisibly, the token that has been saved to the session.
 #' @export
+#' @examples
+#'
+#' \dontrun{
+#' # Set token in environment
+#' Sys.setenv(
+#'   PBI_TENANT = "my_tenant_id",
+#'   PBI_APP = "my_app_id",
+#'   PBI_PW = "my_app_client_secret"
+#'   )
+#'
+#'   pbi_auth()
+#' }
 pbi_auth <- function(tenant = Sys.getenv("PBI_TENANT"),
                      app = Sys.getenv("PBI_APP"),
                      password = Sys.getenv("PBI_PW")) {
 
   .pbi_env$token <- AzureAuth::get_azure_token(
-  resource = "https://analysis.windows.net/powerbi/api",
-  tenant = Sys.getenv("PBI_TENANT"),
-  app = Sys.getenv("PBI_APP"),
-  password = Sys.getenv("PBI_PW"),
-  auth_type = "client_credentials",
-  use_cache = F
+    resource = "https://analysis.windows.net/powerbi/api",
+    tenant = Sys.getenv("PBI_TENANT"),
+    app = Sys.getenv("PBI_APP"),
+    password = Sys.getenv("PBI_PW"),
+    auth_type = "client_credentials",
+    use_cache = F
   )
 }
 
