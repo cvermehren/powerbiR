93e8797736680ab2dc7974a3ee79d88f51e8effb
cvermehren <cv@cantab.net>
2022-01-15 08:29:55 +0000

    Add demo data

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Channel Dimension.
+#'
+#' Anonymized data from google analytics.
+#'
+#' @format A data frame with 10483 rows and 2 variables:
+#' \describe{
+#'   \item{channel_key}{customer ID, factor vector}
+#'   \item{channel_grouping}{transaction date1}
+#'   \item{source}{transaction date2}
+#'   \item{medium}{transaction date3}
+#'   \item{campaign}{transaction date4}
+#'   }
+#'
+#' @source Google Analytics
+"dim_channel"
+
+#' Dim Market.
+#'
+#' Anonymized data from google analytics.
+#'
+#' @format A data frame with 10483 rows and 2 variables:
+#' \describe{
+#'   \item{market_key}{customer ID, factor vector a}
+#'   \item{view_name}{transaction date1 b}
+#'   \item{country}{transaction date2 c}
+#'   }
+#'
+#' @source Google Analytics
+"dim_market"
+
+#' Fact Web Sales.
+#'
+#' Anonymized data from google analytics.
+#'
+#' @format A data frame with 10483 rows and 2 variables:
+#' \describe{
+#'   \item{date}{customer ID, factor vector a}
+#'   \item{market_key}{transaction date2 c}
+#'   \item{channel_key}{transaction date2 c}
+#'   \item{impressions}{transaction date1 b}
+#'   \item{ad_clicks}{transaction date2 c}
+#'   \item{ad_cost}{customer ID, factor vector a}
+#'   \item{sessions}{transaction date1 b}
+#'   \item{transactions}{transaction date2 c}
+#'   \item{revenue}{transaction date2 c}
+#'   }
+#'
+#' @source Google Analytics
+"fact_websales"
