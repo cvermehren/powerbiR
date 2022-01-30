fb8d674e91a973f36e21e962374124d381ca450b
cvermehren <cv@cantab.net>
2022-01-30 15:30:07 +0000

    refresh_hist fun

--- R/datasets.R
+++ R/datasets.R
@@ -36,6 +36,42 @@ pbi_dataset_refresh <- function(group_id,
 
   if (httr::http_error(resp)) {stop(httr::content(resp), call. = FALSE)}
 
+
+
   message("Successfully refreshed dataset with ID ", dataset_id )
+
+  resp[["headers"]][["requestid"]]
+
+}
+
+
+# # Request ID from historical refresh request
+# request_id <- "18af7872-a474-43d8-adce-8c2cffd7d2dc"
+#
+# # Find refresh ID
+# get_index <-function(x) {x$requestId %in% id}
+# request_index <- which(sapply(resp[["value"]], get_index))
+# resp[["value"]][[index]]$requestId
+
+
+pbi_dataset_refresh_hist <- function(group_id, dataset_id, request_id = NULL) {
+
+  token <- pbi_get_token()
+
+  url <- paste0("https://api.powerbi.com/v1.0/myorg/groups/", group_id,"/datasets/", dataset_id, "/refreshes/")
+  url <- utils::URLencode(url)
+
+  header <- httr::add_headers(Authorization = paste("Bearer", token) )
+
+  resp <- httr::GET(url, header)
+
+  if (httr::http_error(resp)) {stop(httr::content(resp), call. = FALSE)}
+
+  #request_id <- resp[["headers"]][["requestid"]]
+
+  resp <- httr::content(resp)
+  resp <- rbindlist(resp$value, fill = TRUE)
+
+  if(!is.null(request_id)) resp[resp$requestId == request_id]$status
 
 }
